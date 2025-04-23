7da87f97a9069750184fc38fa6740369e4ac85a3
TylerYemeni <leomessi111909@gmail.com>
2025-04-24 02:09:08 +0300

    Update auto_connect.py

--- modules/vpn/auto_connect.py
+++ modules/vpn/auto_connect.py
@@ -1,2 +1,6 @@
-def connect_to_fake_vpn():
-    print("[VPN] تم الاتصال بعنوان IP وهمي جديد.")
+import random
+
+def connect_to_vpn():
+    fake_ip = f"10.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(1,254)}"
+    print(f"[VPN] تم الاتصال بعنوان IP وهمي جديد.")
+    return fake_ip
