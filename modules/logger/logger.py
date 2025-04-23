1c0e8abc1a2636f36c909a904f1119d4abab16d8
TylerYemeni <leomessi111909@gmail.com>
2025-04-24 01:18:51 +0300

    Update logger.py

--- modules/logger/logger.py
+++ modules/logger/logger.py
@@ -4,9 +4,12 @@ def setup_logger():
     logger = logging.getLogger("AbuSalem")
     logger.setLevel(logging.DEBUG)
 
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+    formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s")
 
-    logger.addHandler(handler)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    if not logger.hasHandlers():
+        logger.addHandler(console_handler)
+
     return logger
